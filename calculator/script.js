77e0d72ba55a5c79f62ea638b2731b053cb84740
nova14 <saeedghanioour1380@gmail.com>
2021-10-26 18:15:17 +0330

    connect some button to calculator but It's not enough

--- calculator/script.js
+++ calculator/script.js
@@ -1,2 +1,28 @@
+const btn = document.getElementById('btn');
+const inputShow = document.getElementById('show');
+const inputHis = document.getElementById('history');
+const equal = document.getElementById('equal');
+const plus = document.getElementById('plus');
+var totalequal = 0;
+console.log(btn,inputShow,inputHis);
 
+btn.addEventListener("click", () => {
+    
+    console.log('clicked');
+    if ( inputShow.value == 0) {
+         inputShow.value = "";
+    }
+    inputShow.value += 2;
+    
+    console.log('increas');
+});
+
+plus.addEventListener('click', () => {
+
+    inputHis.value += inputShow.value + '+';
+    totalequal += inputShow.value;
+    inputShow.value = 0;
+});
+equal.addEventListener('click', () => {
+    inputShow.value = totalequal;
 })
