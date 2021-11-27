e3661eb4add8625b8cc53e0e73130d90cd7e5b2b
nova14 <saeedghanioour1380@gmail.com>
2021-11-27 11:55:07 +0330

    add some function in this and have some errore for show equal

--- calculator/script.js
+++ calculator/script.js
@@ -1,20 +1,45 @@
-const btn = document.getElementById('btn');
+
 const inputShow = document.getElementById('show');
 const inputHis = document.getElementById('history');
 const equal = document.getElementById('equal');
 const plus = document.getElementById('plus');
-var totalequal = 0;
-console.log(btn,inputShow,inputHis);
+const Ac = document.getElementById('Ac');
+const refresh = document.getElementById('refresh');
+const mines = document.getElementById('mines');
 
-function addNum () {
-    
+var totalequal = 0;
+
+function addNum (number) {
+
+    if(inputShow.value == 0)
+        inputShow.value = number;
+       else
+        inputShow.value += number;
 }
+mines.addEventListener('click', () =>{
+    inputHis.value += inputShow.value + '-';
+    totalequal -= parseInt(inputShow.value);
+    inputShow.value = 0;
+});
+refresh.addEventListener('click', () =>{
+     inputShow.value = 0;
+     inputHis.value = "";
+});
+Ac.addEventListener('click', () =>{
+    inputShow.value = 0;
+});
 plus.addEventListener('click', () => {
 
     inputHis.value += inputShow.value + '+';
-    totalequal += inputShow.value;
+    totalequal += parseInt(inputShow.value);
     inputShow.value = 0;
 });
+
 equal.addEventListener('click', () => {
-    inputShow.value = totalequal;
+    if(inputShow.value == 0)
+       inputShow.value = totalequal;
+    else{
+    
+        inputShow.value = totalequal;
+    }
 })
